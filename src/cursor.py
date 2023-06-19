2a817aaab317e62205e81f6bf303bd121cc8cef9
Callie <collinp.gary32@gmail.com>
2023-06-19 09:00:47 -0700

    Cursor design notes
    Added notes for intended basic functionality of the cursor

--- src/cursor.py
+++ src/cursor.py
@@ -8,3 +8,11 @@ class Cursor:
 
     def is_enemy(self) -> bool:
         return False
+#[Cursor] for moving around the screen, selecting units/tiles. Main controlable element during gameplay. 
+#Cursor should move along the grid by default, able to be sped up/freed from the grid by holding down a button 
+#If the cursor is over a unit, display that unit's stats on the frame
+#If the cursor is over terrain, display any effects on the frame 
+#When the player presses the "confirm" button, the cursor's target should be checked
+#If the target is an empty tile or a player unit which has already moved, open the "Empty" menu from the [Menu] class (An empty tile is one which has no Units on it)
+#If the target is a player unit which has not already moved, open the "Player" menu
+#If the target is an enemy unit, open the "Enemy" menu
